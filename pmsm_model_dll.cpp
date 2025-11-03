331847a9d62f2b36537f16e3b4b486bbedef847f
Luis <luisdaniel11796@gmail.com>
2025-11-02 19:34:45 -0600

    Added copyright notice.

--- pmsm_model_dll.cpp
+++ pmsm_model_dll.cpp
@@ -1,5 +1,20 @@
-// Automatically generated C++ file on Sun Aug 17 19:22:51 2025
-//
+// PMSM model for QSPICE
+// Copyright (C) 2025 Luis Medina
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, see
+<https://www.gnu.org/licenses/>.
+*/
 // To build with Digital Mars C++ Compiler: 
 //
 //    dmc -mn -WD pmsm_model_dll.cpp kernel32.lib
